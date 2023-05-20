4973e10bc2a07cc694a55028b568a755748385a4
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-20 16:37:31 +0300

    initial commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
-
+class CashRegister
+    attr_accessor :total, :employee_discount
+  
+    def initialize(employee_discount = 0)
+      @total = 0
+      @employee_discount = employee_discount
+      @items = []
+    end
+  
+    def add_item(title, price, quantity = 1)
+      item_total = price * quantity
+      @total += item_total
+      quantity.times { @items << { title: title, price: price } }
+    end
+  
+    def apply_discount
+      if @employee_discount > 0
+        discount_amount = (@total * @employee_discount / 100).round(2)
+        @total -= discount_amount
+        "After the discount, the total comes to $#{@total}."
+      else
+        "There is no discount to apply."
+      end
+    end
+  
+    def items
+      @items.map { |item| item[:title] }
+    end
+  
+    def void_last_transaction
+      last_item = @items.pop
+      item_total = last_item[:price]
+      @total -= item_total
+      @total = 0.0 if @items.empty?
+    end
+  end
+  
